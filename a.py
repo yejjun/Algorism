35e8ef3918df40400afc5bdd66c6cea227518585
Jun <136561397+yejjun@users.noreply.github.com>
2024-03-14 10:25:46 +0900

    Update a.py

--- a.py
+++ a.py
@@ -1,26 +1,29 @@
 Width, Height = map(int, input().split())
-tmap, location, visted, arrive = []
+tmap, location, visited, arrive = []
 x, y, cnt = 0
 
-
-def corret(corret_y, corret_x):
+def corret(corret_y, corret_x): # 도착지점 도착시 arrive에 추가
     if location[corret_y][corret_x] == 2:
         arrive.append(cnt)
 
-def right_move(Rmove_y, Rmove_x):
+def right_move(Rmove_y, Rmove_x): 
     temp_x = x
     temp_y = y
     if tmap[temp_y][temp_x + 1] == 0:
         temp_x += 1
         down_move(temp_y, temp_x)
     elif tmap[temp_y][temp_x + 1] == 1:
-        
-
+        if tmap[temp_y+1][temp_x + 1] == 0:
+            temp_y += 1
+            temp_x += 1
+        elif tmap[temp_y][temp_x + 1] == 1:
+            visited.append(tmap[temp_y][temp_x+1])
 
 def down_move(down_y, down_x):
-
-
-
+    while True:
+        if tmap[down_y-1][down_x] == 0: # 만약 아래 블록이 0이면 아래로 한 칸
+            down_y -= 1
+        elif tmap[down_y-1][down_x] == 1:
 
 
 def nevi(location):
@@ -29,6 +32,11 @@ def nevi(location):
             if tmap[y-1][x+1] == 1:
         if tmap[y-1][x] == 0:
             y -= 1
+
+def move(temp_x, temp_y)
+    
+    if d=
+    
 
 
 
